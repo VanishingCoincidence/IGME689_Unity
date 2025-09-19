4c10f59ce127bc95f5cb7dc4f801111597313d08
Esther Loo <89890195+VanishingCoincidence@users.noreply.github.com>
2025-09-19 11:45:30 -0400

    STRUGGLING

--- Assignment2/Assets/Scripts/ReadCSV.cs
+++ Assignment2/Assets/Scripts/ReadCSV.cs
@@ -1,7 +1,8 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEngine;
+using static ReadCSV;
 
 public class ReadCSV : MonoBehaviour
 {
@@ -21,10 +22,8 @@ public class ReadCSV : MonoBehaviour
         public float latitude;
         public float longitude;
         public float confirmedCasesAmt;
-        public float recoveredAmt;
         public float deathsAmt;
-        public float activeCasesAmt;
-        public string admin;
+        public string county;
         public float fips;
         public float incidentRate;
     }
@@ -33,11 +32,12 @@ public class ReadCSV : MonoBehaviour
     public class COVIDCaseList
     {
         public COVIDCase[] covidCase;
+        public int size;
     }
 
     public COVIDCaseList covidCaseList = new COVIDCaseList();
 
-    void Start()
+    void Awake()
     {
         SaveCSVData();
     }
@@ -48,25 +48,25 @@ public class ReadCSV : MonoBehaviour
 
         int tableSize = data.Length / dataColumns - 1;
         covidCaseList.covidCase = new COVIDCase[tableSize];
+        covidCaseList.size = 0;
 
         for (int i = 0; i < tableSize; i++)
         {
+            covidCaseList.size++;
             covidCaseList.covidCase[i] = new COVIDCase();
             covidCaseList.covidCase[i].x = float.Parse(data[dataColumns * (i+ 1)]);
             covidCaseList.covidCase[i].y = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].id = data[dataColumns * (i + 1) + 1];
-            covidCaseList.covidCase[i].state = data[dataColumns * (i + 1) + 1];
-            covidCaseList.covidCase[i].country = data[dataColumns * (i + 1) + 1];
-            covidCaseList.covidCase[i].lastUpdate = data[dataColumns * (i + 1) + 1];
-            covidCaseList.covidCase[i].latitude = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].longitude = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].confirmedCasesAmt = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].recoveredAmt = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].deathsAmt = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].activeCasesAmt = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].admin = data[dataColumns * (i + 1) + 1];
-            covidCaseList.covidCase[i].fips = float.Parse(data[dataColumns * (i + 1) + 1]);
-            covidCaseList.covidCase[i].incidentRate = float.Parse(data[dataColumns * (i + 1) + 1]);
+            covidCaseList.covidCase[i].id = data[dataColumns * (i + 1) + 2];
+            covidCaseList.covidCase[i].state = data[dataColumns * (i + 1) + 3];
+            covidCaseList.covidCase[i].country = data[dataColumns * (i + 1) + 4];
+            covidCaseList.covidCase[i].lastUpdate = data[dataColumns * (i + 1) + 5];
+            covidCaseList.covidCase[i].latitude = float.Parse(data[dataColumns * (i + 1) + 6]);
+            covidCaseList.covidCase[i].longitude = float.Parse(data[dataColumns * (i + 1) + 7]);
+            covidCaseList.covidCase[i].confirmedCasesAmt = float.Parse(data[dataColumns * (i + 1) + 8]);
+            covidCaseList.covidCase[i].deathsAmt = float.Parse(data[dataColumns * (i + 1) + 9]);
+            covidCaseList.covidCase[i].county = data[dataColumns * (i + 1) + 10];
+            covidCaseList.covidCase[i].fips = float.Parse(data[dataColumns * (i + 1) + 11]);
+            covidCaseList.covidCase[i].incidentRate = float.Parse(data[dataColumns * (i + 1) + 12]);
 
         }
     }
--- Assignment2/Assets/Scripts/ShowData.cs
+++ Assignment2/Assets/Scripts/ShowData.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Esri.ArcGISMapsSDK.Components;
+using Esri.GameEngine.Geometry;
+using Esri.HPFramework;
+using Unity.Mathematics;
 
 public class ShowData : MonoBehaviour
 {
+    public ReadCSV readCSV;
+    private ArcGISMapComponent mapComponent;
+
+    public Button confirmedCasesButton;
+
+    public GameObject sphere;
+
+    void Awake()
+    {
+        mapComponent = FindFirstObjectByType<ArcGISMapComponent>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SpawnSpheres();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void SpawnSpheres()
+    {
+        for (int i = 0; i < readCSV.covidCaseList.size; i++)
+        {
+            // chose 100 to be over top of the map
+            //double3 position = new double3((double)readCSV.covidCaseList.covidCase[i].latitude, (double)readCSV.covidCaseList.covidCase[i].longitude, 100);
+            //double3 worldPosition = math.inverse(mapComponent.WorldMatrix).HPTransform(position);
+            //ArcGISPoint geoPosition = mapComponent.View.WorldToGeographic(worldPosition);
+            //ArcGISPoint offsetPosition = new ArcGISPoint(geoPosition.X, geoPosition.Y, geoPosition.Z, geoPosition.SpatialReference);
+            ArcGISPoint point = new ArcGISPoint(readCSV.covidCaseList.covidCase[i].longitude, readCSV.covidCaseList.covidCase[i].latitude, 500, ArcGISSpatialReference.WGS84());
+            double3 position = mapComponent.View.GeographicToWorld(point);
+
+
+            Instantiate(sphere, 
+                new Vector3((float)position.x, (float)position.y, (float)position.z), 
+                transform.rotation,
+                transform);
+
+            Debug.Log(readCSV.covidCaseList.covidCase[i].longitude + ","+
+                readCSV.covidCaseList.covidCase[i].latitude);
+        }
+    }
+
+    public void ShowConfirmedCases()
+    {
+        for(int i = 0; i < readCSV.covidCaseList.size; i++)
+        {
+
+        }
         
     }
 }
